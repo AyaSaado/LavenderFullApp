09f930bd865b61dbc02bb9b98be29a74c4641984
ayasaado188 <ayasaado188@gmail.com>
2024-05-25 18:31:17 +0300

    New Service

---
+++ Lavender.Services/ProductionEmps/Queries/GetEmpsOfManager/GetEmpsOfManagerHandler.cs
@@ -0,0 +1,24 @@
+﻿using Lavender.Core.Interfaces.Repository;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using static Lavender.Services.ProductionEmps.Queries.GetAll.GetAllProductionEmpRequest;
+
+namespace Lavender.Services.ProductionEmps.Queries.GetEmpsOfManager
+{
+    public class GetEmpsOfManagerHandler : IRequestHandler<GetEmpsOfManagerRequest, List<ProductionEmpResponse>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetEmpsOfManagerHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<List<ProductionEmpResponse>> Handle(GetEmpsOfManagerRequest request, CancellationToken cancellationToken)
+        {
+            return  await _unitOfWork.ProductionEmps.Find(p => p.Head.Id == request.ManagerId)
+                                                    .Select(ProductionEmpResponse.Selector())
+                                                    .ToListAsync(cancellationToken);
+        }
+    }
+}
---
+++ Lavender.Services/ProductionEmps/Queries/GetEmpsOfManager/GetEmpsOfManagerRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using static Lavender.Services.ProductionEmps.Queries.GetAll.GetAllProductionEmpRequest;
+
+namespace Lavender.Services.ProductionEmps.Queries.GetEmpsOfManager
+{
+    public class GetEmpsOfManagerRequest : IRequest<List<ProductionEmpResponse>>
+    {
+        public Guid ManagerId { get; set; }
+    }
+}
