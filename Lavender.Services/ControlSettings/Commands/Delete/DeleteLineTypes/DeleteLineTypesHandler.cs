9f1a66f2fa96817ff744a597aa634158b981bbca
ayasaado188 <ayasaado188@gmail.com>
2024-05-30 12:19:00 +0300

    New Service for Line Type

---
+++ Lavender.Services/ControlSettings/Commands/Delete/DeleteLineTypes/DeleteLineTypesHandler.cs
@@ -0,0 +1,36 @@
+﻿using Lavender.Core.Entities;
+using Lavender.Core.Interfaces.Repository;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lavender.Services.ControlSettings.Commands.Delete.DeleteLineTypes
+{
+    public class DeleteLineTypesHandler : IRequestHandler<DeleteLineTypesRequest, bool>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ICRUDRepository<LineType> _lineTyperepository;
+
+        public DeleteLineTypesHandler(ICRUDRepository<LineType> lineTyperepository, IUnitOfWork unitOfWork)
+        {
+            _lineTyperepository = lineTyperepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> Handle(DeleteLineTypesRequest request, CancellationToken cancellationToken)
+        {
+          var entities = await _lineTyperepository.Find(l=> request.Ids.Contains(l.Id))
+                                                  .ToListAsync(cancellationToken);
+
+          try
+          {
+                _lineTyperepository.RemoveRange(entities);
+                await _unitOfWork.Save(cancellationToken);
+                return true;
+          }
+          catch(Exception)
+          {
+                return false;
+          }
+        }
+    }
+}
---
+++ Lavender.Services/ControlSettings/Commands/Delete/DeleteLineTypes/DeleteLineTypesRequest.cs
@@ -0,0 +1,10 @@
+﻿
+using MediatR;
+
+namespace Lavender.Services.ControlSettings.Commands.Delete.DeleteLineTypes
+{
+    public class DeleteLineTypesRequest : IRequest<bool>
+    {
+        public List<int> Ids { get; set; } = new List<int>();
+    }
+}
