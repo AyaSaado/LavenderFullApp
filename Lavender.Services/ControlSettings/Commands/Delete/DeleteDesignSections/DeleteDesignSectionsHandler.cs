0fe8fb781268bc4de2285fa7c5556277aac03c24
ayasaado188 <ayasaado188@gmail.com>
2024-05-30 12:18:16 +0300

    New Service for Design Section

---
+++ Lavender.Services/ControlSettings/Commands/Delete/DeleteDesignSections/DeleteDesignSectionsHandler.cs
@@ -0,0 +1,33 @@
+﻿using Lavender.Core.Interfaces.Repository;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lavender.Services.ControlSettings.Commands.Delete.DeleteDesignSections
+{
+    public class DeleteDesignSectionsHandler : IRequestHandler<DeleteDesignSectionsRequest, bool>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteDesignSectionsHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> Handle(DeleteDesignSectionsRequest request, CancellationToken cancellationToken)
+        {
+          var entities  = await _unitOfWork.DesignSections.Find(d=> request.Ids.Contains(d.Id))
+                                                          .ToListAsync(cancellationToken);
+
+            try
+            {
+                _unitOfWork.DesignSections.RemoveRange(entities);
+                await _unitOfWork.Save(cancellationToken);
+                return true;
+            }
+            catch(Exception) 
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Lavender.Services/ControlSettings/Commands/Delete/DeleteDesignSections/DeleteDesignSectionsRequest.cs
@@ -0,0 +1,10 @@
+﻿
+using MediatR;
+
+namespace Lavender.Services.ControlSettings.Commands.Delete.DeleteDesignSections
+{
+    public class DeleteDesignSectionsRequest : IRequest<bool>
+    {
+        public List<int> Ids { get; set; } = new List<int>(); 
+    }
+}
