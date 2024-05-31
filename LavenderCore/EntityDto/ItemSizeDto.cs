d83b62be9bf0755f3370e93053f29029c90b650e
ayasaado188 <ayasaado188@gmail.com>
2024-05-31 23:44:41 +0300

    New Dto

---
+++ LavenderCore/EntityDto/ItemSizeDto.cs
@@ -0,0 +1,12 @@
+﻿using Lavender.Core.Enum;
+
+namespace Lavender.Core.EntityDto
+{
+    public class ItemSizeDto
+    {
+        public int Id { get; set; }
+        public Size Size { get; set; }
+        public ICollection<ItemSizeWithColorDto> ItemSizeWithColorDtos { get; set; } = new List<ItemSizeWithColorDto>();
+
+    }
+}
---
+++ LavenderCore/EntityDto/ItemSizeWithColorDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Lavender.Core.EntityDto
+{
+    public class ItemSizeWithColorDto
+    {
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public string Color { get; set; } = null!;
+    }
+}
