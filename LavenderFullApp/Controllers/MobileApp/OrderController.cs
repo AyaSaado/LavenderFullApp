7d5f0522326e7a557fd196c236346a13e4184130
ayasaado188 <ayasaado188@gmail.com>
2024-05-31 23:46:17 +0300

    Fix controller's name

---
+++ LavenderFullApp/Controllers/MobileApp/OrderController.cs
@@ -0,0 +1,51 @@
+﻿using Lavender.Services.Orders.Command.Add;
+using Lavender.Services.Orders.Command.Delete;
+using Lavender.Services.Orders.Command.Update;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace LavenderFullApp.Controllers.MobileApp
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "MobileApp")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("AddOrder")]
+        [SwaggerResponse(StatusCodes.Status200OK, null, typeof(bool))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Add([FromBody] AddOrderRequest command, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(command, cancellationToken);
+            return result ? Ok() : BadRequest();
+        }
+
+        [HttpPut("UpdateOrder")]
+        [SwaggerResponse(StatusCodes.Status200OK, null, typeof(bool))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update([FromBody] UpdateOrderRequest command, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(command, cancellationToken);
+            return result ? Ok() : BadRequest();
+        }
+
+
+        [HttpDelete("DeleteOrder")]
+        [SwaggerResponse(StatusCodes.Status204NoContent, null, typeof(bool))]
+        [SwaggerResponse(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete([FromQuery] DeleteOrderRequest command, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(command, cancellationToken);
+            return result ? NoContent() : NotFound();
+        }
+
+    }
+}
