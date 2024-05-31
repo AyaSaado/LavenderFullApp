1a650f4cac4e8a4c9ffb01541ee008ed3beb89e8
ayasaado188 <ayasaado188@gmail.com>
2024-05-31 23:47:29 +0300

    Add New Repository

--- LavenderCore/Interfaces/Repository/IUnitOfWork.cs
+++ LavenderCore/Interfaces/Repository/IUnitOfWork.cs
@@ -7,6 +7,7 @@
         IMakerSectionRepository MakerSections { get; }
         IProductionEmpRepository ProductionEmps { get; } 
         IPatternMakerRepository PatternMakers { get; }
+        IOrderRepository Orders { get; }
         Task<int> Save(CancellationToken cancellationToken);
     }
 }
