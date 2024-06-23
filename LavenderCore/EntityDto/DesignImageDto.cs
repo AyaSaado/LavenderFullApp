1153f23adf01eede4e4c8c8c2add45b3aa315514
ayasaado188 <ayasaado188@gmail.com>
2024-06-23 16:12:07 +0300

    New Dto

---
+++ LavenderCore/EntityDto/DesignImageDto.cs
@@ -0,0 +1,12 @@
+﻿using Lavender.Core.Enum;
+
+namespace Lavender.Core.EntityDto
+{
+    public class DesignImageDto
+    {
+        public int Id { get; set; }
+        public string? Url { get; set; }
+        public string? Description { get; set; } 
+        public ImageType ImageType { get; set; }
+    }
+}
---
+++ LavenderCore/EntityDto/FabricDesignDto.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Lavender.Core.EntityDto
+{
+    public class FabricDesignDto
+    {
+        public int Id { get; set; }
+        public decimal FabricWidth { get; set; }
+        public decimal FabricHeight { get; set; }
+        public string? Purpose { get; set; }
+        public string Color { get; set; } = null!; 
+        public int FabricTypeId { get; set; }
+
+    }
+}
