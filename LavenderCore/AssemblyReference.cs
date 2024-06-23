cf0fe4fe09612528ee3ec465da81f037f67a96c2
ayasaado188 <ayasaado188@gmail.com>
2024-06-23 16:13:03 +0300

    Added

---
+++ LavenderCore/AssemblyReference.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace Lavender.Core
+{
+    public class AssemblyReference
+    {
+        public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+    }
+}
