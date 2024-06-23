3c42e014dd8e9cbe8fefe72a4abda1810a59a4f2
ayasaado188 <ayasaado188@gmail.com>
2024-06-23 15:58:50 +0300

    Add Fabrics Route

---
+++ Lavender.Services/ControlSettings/Commands/Add/AddFabrics/AddFabricsRequest.cs
@@ -0,0 +1,11 @@
+﻿
+
+using MediatR;
+
+namespace Lavender.Services.ControlSettings
+{
+    public class AddFabricsRequest : IRequest<bool>
+    {
+        public List<string> FabricTypesName { get; set; } = new List<string>();
+    }
+}
