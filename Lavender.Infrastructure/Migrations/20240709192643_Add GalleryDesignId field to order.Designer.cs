3667b20eee629001a6eff7fa898b79b4d134ac9a
ayasaado188 <ayasaado188@gmail.com>
2024-07-09 22:56:39 +0300

    Updated

--- Lavender.Infrastructure/Migrations/20240709192643_Add GalleryDesignId field to order.Designer.cs
+++ Lavender.Infrastructure/Migrations/20240709192643_Add GalleryDesignId field to order.Designer.cs
@@ -4,6 +4,7 @@ using Lavender.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Lavender.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240709192643_Add GalleryDesignId field to order")]
+    partial class AddGalleryDesignIdfieldtoorder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Lavender.Infrastructure/Migrations/20240709192643_Add GalleryDesignId field to order.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lavender.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGalleryDesignIdfieldtoorder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GalleryDesignId",
+                table: "Order",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GalleryDesignId",
+                table: "Order");
+        }
+    }
+}
