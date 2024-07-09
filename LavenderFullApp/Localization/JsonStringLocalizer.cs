550ae1868826b24f72c395c3f2f02aa45bec850b
ayasaado188 <ayasaado188@gmail.com>
2024-07-09 19:56:24 +0300

    New Service Of Api Responses' Translation

---
+++ LavenderFullApp/Localization/JsonStringLocalizer.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Localization;
+using Newtonsoft.Json;
+using System.Text;
+
+
+namespace LavenderFullApp.Localization
+{
+    public class JsonStringLocalizer : IStringLocalizer
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IDistributedCache _cache;
+        private readonly JsonSerializer _serializer = new();
+
+        public JsonStringLocalizer(IDistributedCache cache, IHttpContextAccessor httpContextAccessor)
+        {
+            _cache = cache;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public LocalizedString this[string name]
+        {
+            get
+            {
+                var value = GetString(name);
+                return new LocalizedString(name, value);
+            }
+        }
+
+        public LocalizedString this[string name, params object[] arguments]
+        {
+            get
+            {
+                var actualValue = this[name];
+                return !actualValue.ResourceNotFound
+                    ? new LocalizedString(name, string.Format(actualValue.Value, arguments))
+                    : actualValue;
+            }
+        }
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+
+            var langHeaderValue = httpContext.Request.Headers["Lang"].ToString();
+
+            var filePath = $"Resources/{langHeaderValue}.json";
+
+            using FileStream stream = new(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            using StreamReader streamReader = new(stream);
+            using JsonTextReader reader = new(streamReader);
+
+            while (reader.Read())
+            {
+                if (reader.TokenType != JsonToken.PropertyName)
+                    continue;
+
+                var key = reader.Value as string;
+                reader.Read();
+                var value = _serializer.Deserialize<string>(reader);
+                yield return new LocalizedString(key!, value!);
+            }
+        }
+
+        private string GetString(string key)
+        {
+            
+            var httpContext = _httpContextAccessor.HttpContext;
+           
+            var langHeaderValue = httpContext.Request.Headers["Lang"].ToString();
+          
+            var fullFilePath = $"Resources/{langHeaderValue}.json";
+            if (File.Exists(fullFilePath))
+            {
+                var cacheKey = $"locale_{langHeaderValue}_{key}";
+                var cacheValue = _cache.GetString(cacheKey);
+
+                if (!string.IsNullOrEmpty(cacheValue))
+                    return cacheValue;
+
+                var result = GetValueFromJSON(key, fullFilePath);
+
+                if (!string.IsNullOrEmpty(result))
+                    _cache.SetString(cacheKey, result);
+
+                return result;
+            }
+
+            return string.Empty;
+        }
+
+        private string GetValueFromJSON(string propertyName, string filePath)
+        {
+            if (string.IsNullOrEmpty(propertyName) || string.IsNullOrEmpty(filePath))
+                return string.Empty;
+
+            using FileStream stream = new(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            using StreamReader streamReader = new(stream);
+            using JsonTextReader reader = new(streamReader);
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonToken.PropertyName && reader.Value as string == propertyName)
+                {
+                    reader.Read();
+                    return _serializer.Deserialize<string>(reader)!;
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ LavenderFullApp/Localization/JsonStringLocalizerFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Localization;
+
+namespace LavenderFullApp.Localization
+{
+    public class JsonStringLocalizerFactory : IStringLocalizerFactory
+    {
+        private readonly IDistributedCache _cache;
+          private readonly IHttpContextAccessor _httpContextAccessor;
+        public JsonStringLocalizerFactory(IDistributedCache cache, IHttpContextAccessor httpContextAccessor)
+        {
+            _cache = cache;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public IStringLocalizer Create(Type resourceSource)
+        {
+            return new JsonStringLocalizer(_cache, _httpContextAccessor);
+        }
+
+        public IStringLocalizer Create(string baseName, string location)
+        {
+            return new JsonStringLocalizer(_cache, _httpContextAccessor);
+        }
+    }
+
+}
