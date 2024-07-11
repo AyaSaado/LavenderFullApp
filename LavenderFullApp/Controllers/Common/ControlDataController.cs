7bb9e95d1dc0d961b99b2e7ac8575e8d180e0525
ayasaado188 <ayasaado188@gmail.com>
2024-07-12 02:25:33 +0300

    New Endpoint

--- LavenderFullApp/Controllers/Common/ControlDataController.cs
+++ LavenderFullApp/Controllers/Common/ControlDataController.cs
@@ -39,8 +39,16 @@ namespace LavenderFullApp.Controllers.Common
         }
 
         [HttpGet("GetAllItems")]
-        [SwaggerResponse(StatusCodes.Status200OK, null, typeof(ItemsResponse))]
+        [SwaggerResponse(StatusCodes.Status200OK, null, typeof(List<ItemResponse>))]
         public async Task<IActionResult> GetAll([FromQuery] GetAllItemsRequest request, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(request, cancellationToken);
+            return Ok(result);
+        }
+
+        [HttpGet("GetItemById")]
+        [SwaggerResponse(StatusCodes.Status200OK, null, typeof(ItemResponse))]
+        public async Task<IActionResult> Get([FromQuery] GetItemByIdRequest request, CancellationToken cancellationToken)
         {
             var result = await _mediator.Send(request, cancellationToken);
             return Ok(result);
@@ -56,7 +64,7 @@ namespace LavenderFullApp.Controllers.Common
 
 
         [HttpGet("GetAllItemTypesOfStoreRequest")]
-        [SwaggerResponse(StatusCodes.Status200OK, null, typeof(STypeResponse))]
+        [SwaggerResponse(StatusCodes.Status200OK, null, typeof(List<STypeResponse>))]
         public async Task<IActionResult> GetAll([FromQuery] GetAllSTypesRequest request, CancellationToken cancellationToken)
         {
             var result = await _mediator.Send(request, cancellationToken);
@@ -65,12 +73,20 @@ namespace LavenderFullApp.Controllers.Common
 
 
         [HttpGet("GetAllStoreItems")]
-        [SwaggerResponse(StatusCodes.Status200OK, null, typeof(ControlData))]
+        [SwaggerResponse(StatusCodes.Status200OK, null, typeof(List<ControlData>))]
         public async Task<IActionResult> GetAll([FromQuery] GetAllStoreItemsRequest request, CancellationToken cancellationToken)
         {
             var result = await _mediator.Send(request, cancellationToken);
             return Ok(result);
         }
 
+
+        [HttpGet("GetItemDetails")]
+        [SwaggerResponse(StatusCodes.Status200OK, null, typeof(List<ItemDetailResponse>))]
+        public async Task<IActionResult> GetAll([FromQuery] GetItemDetailsRequest request, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(request, cancellationToken);
+            return Ok(result);
+        }
     }
 }
