08ece96e05bc69b79fa06c983308a71e99e7812d
ayasaado188 <ayasaado188@gmail.com>
2024-07-12 02:23:59 +0300

    New Dto

---
+++ LavenderCore/EntityDto/ConsumingDto.cs
@@ -0,0 +1,14 @@
+﻿using Lavender.Core.Enum;
+
+namespace Lavender.Core.EntityDto
+{
+    public class ConsumingDto
+    {
+        public int Id { get; set; }
+        public decimal QuantityOrdered { get; set; }
+        public TypeOfUnit TypeOfUnit { get; set; }
+        public DateOnly DateOfDemand { get; set; }
+        public int OrderId { get; set; }
+        public int SItemTypeId { get; set; }
+    }
+}
---
+++ LavenderCore/EntityDto/PlanDto.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Lavender.Core.EntityDto
+{
+    public class PlanDto
+    {
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public int StepId { get; set; }
+    }
+}
