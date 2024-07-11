c1cfb8cc2a7d5ba23183e8d4e42ae72af053f8ca
ayasaado188 <ayasaado188@gmail.com>
2024-07-12 02:20:52 +0300

    New Service Command

---
+++ Lavender.Services/Orders/Command/AddRawItems/AddRawItemsOfOrderHandler.cs
@@ -0,0 +1,34 @@
+﻿using Lavender.Core.Entities;
+using Lavender.Core.Interfaces.Repository;
+using MediatR;
+using static Lavender.Core.Helper.MappingProfile;
+
+namespace Lavender.Services.Orders
+{
+    public class AddRawItemsOfOrderHandler : IRequestHandler<AddRawItemsOfOrderRequest, bool>
+    {
+        private readonly ICRUDRepository<Consuming> _consumingRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public AddRawItemsOfOrderHandler(ICRUDRepository<Consuming> consumingRepository, IUnitOfWork unitOfWork)
+        {
+            this._consumingRepository = consumingRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> Handle(AddRawItemsOfOrderRequest request, CancellationToken cancellationToken)
+        {
+            var entities = Mapping.Mapper.Map<List<Consuming>>(request.ConsumingDtos);
+
+            try
+            {
+                await _consumingRepository.AddRangeAsync(entities);
+                await _unitOfWork.Save(cancellationToken);
+                return true;
+            }
+            catch(Exception)
+            { 
+                return false;
+            }
+        }
+    }
+}
---
+++ Lavender.Services/Orders/Command/AddRawItems/AddRawItemsOfOrderRequest.cs
@@ -0,0 +1,11 @@
+﻿using Lavender.Core.EntityDto;
+using MediatR;
+
+namespace Lavender.Services.Orders
+{
+    public class AddRawItemsOfOrderRequest : IRequest<bool>
+    {
+      public List<ConsumingDto> ConsumingDtos {  get; set; } = new List<ConsumingDto>();
+
+    }
+}
