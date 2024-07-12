4b679d97020a5ea509c983936afcc9fee92182a3
ayasaado188 <ayasaado188@gmail.com>
2024-07-12 03:31:47 +0300

    New

---
+++ LavenderCore/EntityDto/PaymentDto.cs
@@ -0,0 +1,21 @@
+﻿using Lavender.Core.Entities;
+using System.Linq.Expressions;
+
+
+namespace Lavender.Core.EntityDto
+{
+    public class PaymentDto
+    {
+        public int Id { get; set; }
+        public decimal PaidMoney { get; set; }
+        public DateOnly PayingDate { get; set; }
+        public static Expression<Func<Payment, PaymentDto>> Selector() => c
+         => new()
+         {
+             Id = c.Id,
+             PaidMoney = c.PaidMoney,
+             PayingDate = c.PayingDate,
+         };
+
+    }
+}
