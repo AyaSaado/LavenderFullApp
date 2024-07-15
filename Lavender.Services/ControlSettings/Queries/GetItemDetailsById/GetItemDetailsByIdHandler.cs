a7139a0165ba6beef9df881070041ac822cca7db
ayasaado188 <ayasaado188@gmail.com>
2024-07-15 14:37:32 +0300

    New Query

---
+++ Lavender.Services/ControlSettings/Queries/GetItemDetailsById/GetItemDetailsByIdHandler.cs
@@ -0,0 +1,24 @@
+﻿using Lavender.Core.Entities;
+using Lavender.Core.Interfaces.Repository;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lavender.Services.ControlSettings
+{
+    public class GetItemDetailsByIdHandler : IRequestHandler<GetItemDetailsByIdRequest, ItemDetailResponse?>
+    {
+        private readonly ICRUDRepository<SItemType> _sItemTypeRepository;
+
+        public GetItemDetailsByIdHandler(ICRUDRepository<SItemType> sItemTypeRepository)
+        {
+            _sItemTypeRepository = sItemTypeRepository;
+        }
+
+        public async Task<ItemDetailResponse?> Handle(GetItemDetailsByIdRequest request, CancellationToken cancellationToken)
+        {
+            return await _sItemTypeRepository.Find(i=>i.Id == request.SItemTypeId)
+                                             .Select(ItemDetailResponse.Selector())
+                                             .FirstOrDefaultAsync(cancellationToken);
+        }
+    }
+}
---
+++ Lavender.Services/ControlSettings/Queries/GetItemDetailsById/GetItemDetailsByIdRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+
+namespace Lavender.Services.ControlSettings
+{
+    public class GetItemDetailsByIdRequest : IRequest<ItemDetailResponse?>
+    {
+        public int SItemTypeId { get; set; }
+    }
+
+            
+}
