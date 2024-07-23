0884908eb425213aeb90a193f54b999db53a8f83
ayasaado188 <ayasaado188@gmail.com>
2024-07-24 02:38:00 +0300

    Fixed

--- Lavender.Services/Users/Commands/ForgetPassword/ForgetPasswordHandler.cs
+++ Lavender.Services/Users/Commands/ForgetPassword/ForgetPasswordHandler.cs
@@ -19,20 +19,27 @@ namespace Lavender.Services.Users
 
         public async Task<string> Handle(ForgetPasswordRequest request, CancellationToken cancellationToken)
         {
-            var user = await _userMangerRepository.FindByEmailAsync(request.Email);     
-            if (user == null) return "";
-
-            var code =  _random.Next(1000, 9999).ToString("D4");
-
-            var emailRequest = new Mailrequest()
+            try
             {
-                ToEmail = request.Email,
-                Subject = "Forget Password Confirm Code, Lavender App",
-                Body = code
-                
-            };
-            await _emailRepository.SendEmailAsync(emailRequest);
-            return  code;
+                var user = await _userMangerRepository.FindByEmailAsync(request.Email);
+                if (user == null) return string.Empty;
+
+                var code = _random.Next(1000, 9999).ToString("D4");
+
+                var emailRequest = new Mailrequest()
+                {
+                    ToEmail = request.Email,
+                    Subject = "Forget Password Confirm Code, Lavender App",
+                    Body = code
+
+                };
+                await _emailRepository.SendEmailAsync(emailRequest);
+                return code;
+            }
+            catch(Exception)
+            {
+                return string.Empty;
+            }
         }
     }
 }
