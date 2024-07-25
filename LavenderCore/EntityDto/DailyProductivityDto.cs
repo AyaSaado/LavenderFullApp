a94237ad902979d0250d0c17681879a7813dccd6
ayasaado188 <ayasaado188@gmail.com>
2024-07-25 15:45:07 +0300

    NewDto

---
+++ LavenderCore/EntityDto/DailyProductivityDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Lavender.Core.EntityDto
+{
+    public class DailyProductivityDto
+    {
+        public int Id { get; set; }
+        public DateOnly Day { get; set; }
+        public TimeSpan WorkHours { get; set; }
+        public int WorkQuantity { get; set; }
+        public int WorkerId { get; set; }
+    }
+}
---
+++ LavenderCore/EntityDto/ModelNameDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Lavender.Core.EntityDto
+{
+    public class ModelNameDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Productivity { get; set; }
+    }
+}
