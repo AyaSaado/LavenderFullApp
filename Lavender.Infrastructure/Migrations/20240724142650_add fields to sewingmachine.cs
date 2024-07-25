4ffc3c4aeba5e4000cfbb0a99150ca2d65b79b87
ayasaado188 <ayasaado188@gmail.com>
2024-07-25 15:35:44 +0300

    Updated

---
+++ Lavender.Infrastructure/Migrations/20240724142650_add fields to sewingmachine.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lavender.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addfieldstosewingmachine : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "SewingMachine",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Productivity",
+                table: "ModelName",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "SewingMachine");
+
+            migrationBuilder.DropColumn(
+                name: "Productivity",
+                table: "ModelName");
+        }
+    }
+}
